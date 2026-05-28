b111a614451a9761483d98da48bac1ceff5a9d2f
Krystohans <krystohans@ebookpirates.hu>
2026-05-28 06:55:04 +0200

    fix(taverna): remove undefined formatAIResponse causing ReferenceError

--- app.js
+++ app.js
@@ -2046,7 +2046,7 @@ function openTavernJobs() {
         if (typeof callBackend === 'function') {
             callBackend("getTavernJobs", [], function(response) {
                 if (response && response.text) {
-                    contentDiv.innerHTML = formatAIResponse(response.text);
+                    contentDiv.innerHTML = response.text;
                 } else {
                     contentDiv.innerHTML = '<p>Az asztalnál lévők csak morognak. (Hiba: ' + (response.error || "Nincs válasz") + ')</p>';
                 }
